48d7dbcf0f58fe187e083902166ebaf13c847ac2
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-02-24 21:40:03 +0530

    Change is testimonial schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -484,6 +484,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
 export interface ApiTestimonialTestimonial extends Struct.CollectionTypeSchema {
   collectionName: 'testimonials';
   info: {
+    description: '';
     displayName: 'Testimonial';
     pluralName: 'testimonials';
     singularName: 'testimonial';
@@ -495,7 +496,6 @@ export interface ApiTestimonialTestimonial extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    Image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',

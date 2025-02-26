19958afe3c863cf1aa3b08d7f97900eec74dc46f
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-02-27 01:56:01 +0530

    updated server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://cms.labeljuilakhani.in',
+  admin: { autoOpen: false },
   app: {
     keys: env.array('APP_KEYS'),
   },
